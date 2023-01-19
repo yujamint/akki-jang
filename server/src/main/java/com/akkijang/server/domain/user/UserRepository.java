7f01d47c91fcc802bebbbf556aa57d2b6e17d5f2
yujaemin <woawoa0513@gmail.com>
2023-01-20 02:12:43 +0900

    feat: email에 따른 User 조회 쿼리 추가

--- server/src/main/java/com/akkijang/server/domain/user/UserRepository.java
+++ server/src/main/java/com/akkijang/server/domain/user/UserRepository.java
@@ -1,6 +1,9 @@
 package com.akkijang.server.domain.user;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }

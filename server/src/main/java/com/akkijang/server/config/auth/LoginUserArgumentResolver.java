85c1049962c59c99958be6b23559b46d2abd934f
yujaemin <woawoa0513@gmail.com>
2023-01-20 14:27:00 +0900

    feat: LoginUser 어노테이션 구현
    세션에서 User 정보를 가져온다. 로그인 된 상태에서만 실행 가능한 메소드에 붙여줌으로써 세션 접근 코드 단축

---
+++ server/src/main/java/com/akkijang/server/config/auth/LoginUserArgumentResolver.java
@@ -0,0 +1,32 @@
+package com.akkijang.server.config.auth;
+
+import com.akkijang.server.config.auth.LoginUser;
+import com.akkijang.server.config.auth.dto.SessionUser;
+import javax.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@RequiredArgsConstructor
+@Component
+public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final HttpSession httpSession;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
+        boolean isUserClas = SessionUser.class.equals(parameter.getParameterType());
+        return isLoginUserAnnotation && isUserClas;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer,
+                                  NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        return httpSession.getAttribute("user");
+    }
+}
